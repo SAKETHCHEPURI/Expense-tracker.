fea1c167fb6a21992c736682e1d1ed121a1ad249
Saketh Chepuri <sakethsaketh795@gmail.com>
2026-06-04 19:48:55 +0530

    update

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -172,3 +172,8 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
 ]
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "expenses" / "static",
+]
--- DjangoProject1/urls.py
+++ DjangoProject1/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView, LogoutView
 from expenses import views
 
 urlpatterns = [
     path('', lambda request: redirect('/login/')),
     path('admin/', admin.site.urls),
 
-    path('', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('signup/', views.signup, name='signup'),
     path('expenses/', include('expenses.urls')),
